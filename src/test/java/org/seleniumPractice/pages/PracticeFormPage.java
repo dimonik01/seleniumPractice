0b1385151fbfcca8d1f944a97e45f852a7235790
anikeenko.d <anikeenko.d@infosysco.ru>
2025-11-17 18:57:27 +0500

    Add getters, new entities

--- src/test/java/org/seleniumPractice/pages/PracticeFormPage.java
+++ src/test/java/org/seleniumPractice/pages/PracticeFormPage.java
@@ -14,39 +14,75 @@ public class PracticeFormPage {
     private By radiobuttonOther = By.id("gender-radio-3");
     private By mobileInput = By.id("userNumber");
     private By submitButton = By.id("submit");
+    private By table = By.cssSelector(".table > tbody");
+
+    public String getFirstNameInputText(){
+        return driver.findElement(firstNameInput).getText();
+    }
+
+    public String getLastNameInputText(){
+        return driver.findElement(lastNameInput).getText();
+    }
+
+    public String getEmailInputText(){
+        return driver.findElement(emailInput).getText();
+    }
+
+    public boolean getRadiobuttonMaleStatus(){
+        return driver.findElement(radiobuttonMale).isEnabled();
+    }
+
+    public boolean getRadiobuttonFemaleStatus(){
+        return driver.findElement(radiobuttonMale).isEnabled();
+    }
+
+    public boolean getRadiobuttonOtherStatus(){
+        return driver.findElement(radiobuttonMale).isEnabled();
+    }
+
+    public String getMobileInputText(){
+        return driver.findElement(radiobuttonMale).getText();
+    }
+
+    public String getSubmitButton(){
+        return driver.findElement(radiobuttonMale).getText();
+    }
+
+    public By getTable(){
+        return table;
+    }
 
     public PracticeFormPage(WebDriver driver){
         this.driver = driver;
     }
 
-    void fillFirstName(String value){
+    public void fillFirstName(String value){
         driver.findElement(firstNameInput).sendKeys(value);
     }
 
-    void fillLastName(String value){
+    public void fillLastName(String value){
         driver.findElement(lastNameInput).sendKeys(value);
     }
 
-    void fillEmail(String value){
+    public void fillEmail(String value){
         driver.findElement(emailInput).sendKeys(value);
     }
 
-    void selectGender(String gender) throws NullPointerException{
+    public void selectGender(String gender) throws NullPointerException{
         if (gender == "male") driver.findElement(radiobuttonMale).click();
         if (gender == "female") driver.findElement(radiobuttonFemale).click();
         if (gender == "other") driver.findElement(radiobuttonOther).click();
     }
 
-    void fillMobile(String value){
+    public void fillMobile(String value){
         driver.findElement(mobileInput).sendKeys(value);
     }
 
-    void submitForm(){
+    public void submitForm(){
         driver.findElement(submitButton).click();
     }
 
-    public String getSubmittedData(){
-        String data =
-       return data;
+    public void getSubmittedData(){
+        System.out.println(table);
     }
 }
--- src/test/java/org/seleniumPractice/tests/PracticeFormTest.java
+++ src/test/java/org/seleniumPractice/tests/PracticeFormTest.java
@@ -1,4 +1,42 @@
 package org.seleniumPractice.tests;
 
+import dev.failsafe.internal.util.Assert;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.seleniumPractice.pages.PracticeFormPage;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
 public class PracticeFormTest {
+    private WebDriver driver;
+    PracticeFormPage practiceFormPage;
+
+    @BeforeAll
+    static void initBrowser(){
+
+    }
+
+    @BeforeEach
+    void visitPage(){
+        driver = new ChromeDriver();
+        practiceFormPage = new PracticeFormPage(driver);
+        driver.get("https://demoqa.com/automation-practice-form");
+    }
+
+    @AfterEach
+    void teardown(){
+        if (driver != null) driver.quit();
+    }
+
+    @Test
+    void launch(){
+        practiceFormPage.fillFirstName("text");
+        assertThat(practiceFormPage.getFirstNameInputText()).contains("text");
+    }
+
+
 }
